22c75bd58d4ffd86d204381180735c2d6cb1ddc2
calvin <kcwan99@gmail.com>
2023-04-26 15:30:57 +0800

    Task_01 and Task_03

--- task.py
+++ task.py
@@ -42,5 +42,13 @@ if __name__ == "__main__":
     # Task-2 - encode the paragraph by shifting the position of each character by a variable value e.g. I am a boy (1) -> J bn b cpz
 
     # Task-3 - Reverse the entire paragraph line by line e.g. I am a boy -> yob a ma I
+    
+    def reverse_slicing(s):
+        return s[::-1]
+
+    input_str = 'I am a boy'
+
+    if __name__ == "__main__":
+        print('Reverse String using slicing =', reverse_slicing(input_str))
 
     # Task-4 - Reverse the order of character of each word e.g. I am a boy -> I ma a yob
